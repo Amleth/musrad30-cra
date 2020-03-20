f4fa172f87b2d862ce20d77b755bef0980bac7e1
plarocca <pierre.la-rocca@etu.utc.fr>
2020-03-20 12:07:52 +0100

    Mise en place de composants graphique d'un musicien

--- src/components/musician/index.jsx
+++ src/components/musician/index.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
-import MaterialTable from 'material-table'
-import spacing from '@material-ui/system'
+import Table from '@material-ui/core/Typography'
 import { withRouter } from 'react-router'
 import axios from 'axios'
-import { Container } from '@material-ui/core'
+import { Container, Card, CardContent, TableHead, TableRow, TableBody, TableCell } from '@material-ui/core'
 
 class Musician extends React.Component {
   constructor(props) {
@@ -25,32 +24,104 @@ class Musician extends React.Component {
     })
   }
 
- 
+
   render() {
     if (!this.state.data) {
       return <div>Rien…</div>
     } else {
       return (
-        
-        <Container>
 
-          <Box mx="auto">
-            <Typography>
-            Affichage du musicien d'id <code>{this.props.match.params.id}</code> (aller lire{' '}
-            <code>this.state.data</code>)
+        <Container>
+          <Grid>
+            <Typography variant="h5" component="h2">
+              Affichage du musicien d'id <code>{this.props.match.params.id}</code> (aller lire{' '}
+              <code>this.state.data</code>)
             </Typography>
-          </Box>
-          
-          <Box mx="auto">
-            <Grid container Box xs={12} spacing={3}>
-              <Box p={2}>Nom</Box>
-              <Box p={2}>Prenom</Box>
-              <Box p={2}>(Anaissance - Amort)</Box>
-            </Grid>
-          </Box>
-          
+
+            <Box mx="auto">
+              <Grid container direction="row" justify="space-evenly" alignItems="center">
+                <Box p={2}>
+                  Nom
+                </Box>
+                <Box p={2}>Prenom</Box>
+                <Box p={2}>(Anaissance - Amort)</Box>
+              </Grid>
+            </Box>
+          </Grid>
+
+
+          <Grid spacing={4} Box direction="row" >
+            <Box Box display="flex" flexWrap="nowrap" direction="column" justify="space-between" alignItems="flex-start">
+              <Box container direction="column" >
+                <Box m={3}>
+                  <Typography variant="button" component="h3">
+                    statut :
+                    </Typography>
+                </Box>
+                <Box m={3}>
+                  <Typography gutterBottom variant="button" component="h3">
+                    nationalite :
+                  </Typography>
+                </Box>
+                <Box m={3}>
+                  <Typography variant="button" component="h3">
+                    style :
+                    </Typography>
+                </Box>
+              </Box>
+
+              <Box m={3}>
+                <Typography gutterBottom variant="button" component="h3">
+                  Infos :
+                  </Typography>
+                <Typography varaint="body2" color="textSecondary">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non laoreet est.
+                  </Typography>
+              </Box>
+            </Box>
+          </Grid>
+          {/*Si le musicien possede des oeuvres alors les afficher*/}
+          <Grid>
+            <Box m={3}>
+              <Typography variant="h5" component="h3">
+                Oeuvres Composées
+              </Typography>
+            </Box>
+            <Box>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="right">Titre</TableCell>
+                    <TableCell align="right">Informations de diffusion</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+
+                </TableBody>
+              </Table>
+            </Box>
+          </Grid>
+          {/*Si le musicien interprete des oeuvres alors les afficher*/}
+          <Grid>
+            <Box m={3}>
+              <Typography variant="h5" component="h3">
+                Oeuvres Interpretées
+              </Typography>
+            </Box>
+            <Box>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                  <TableCell align="right">Titre</TableCell>
+                    <TableCell align="right">Informations de diffusion</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                </TableBody>
+              </Table>
+            </Box>
+          </Grid>       
         </Container>
-        
       )
     }
   }

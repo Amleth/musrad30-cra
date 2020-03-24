ada508063ccacb4ff2d8269ec7b16c2076aed4df
plarocca <pierre.la-rocca@etu.utc.fr>
2020-03-24 17:23:13 +0100

    Ajout de la gestions de l'index musiciens (avec doublons)

--- src/components/musicians/index.jsx
+++ src/components/musicians/index.jsx
@@ -1,19 +1,46 @@
 import React from 'react'
 import MaterialTable from 'material-table'
 import { withRouter } from 'react-router'
-import { Container, Box } from '@material-ui/core'
+import axios from 'axios'
 
 class Musicians extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Box>
-          <MaterialTable>
+  constructor(props) {
+    super(props)
+    this.state = {
+      musiciansData: null,
+    }
+  }
 
-          </MaterialTable>
-        </Box>
-      </Container>
-    )
+  componentDidMount() {
+    axios.get('http://data-iremus.huma-num.fr/musrad30/musicians/').then(res => {
+      this.setState({ musiciansData: res.data })
+    })
+  }
+
+  render() {
+    if (!this.state.musiciansData) {
+      return <div>Données en cours de téléchargement...</div>
+    } else {
+      return (
+        
+        <div style={{maxWidth: '100%'}}>
+          <MaterialTable
+          title = 'Index des Musiciens'
+          columns={[
+            {title:'Nom', field:'surname'},
+            {title:'Prenom', field:'givenName'},
+            {title:'Status', field:'status_label'},
+            {title:'Nationalté', field:'nationality_label'},
+            {title:'Style', field:'style_label'},
+            {title:'Page associee', field:'style_label'},
+          ]}
+          data = {this.state.musiciansData}
+          >
+
+          </MaterialTable>  
+        </div>
+      )
+    }
   }
 }
 

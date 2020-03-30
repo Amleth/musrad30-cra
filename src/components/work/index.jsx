bbab9231974e5ad9e08f9c6dca228e4c1efbf1e2
plarocca <pierre.la-rocca@etu.utc.fr>
2020-03-30 10:13:02 +0200

    Premiere implementation de work

--- src/components/work/index.jsx
+++ src/components/work/index.jsx
@@ -12,6 +12,7 @@ import {
   TableCell,
   Typography
 } from '@material-ui/core'
+import MaterialTable from 'material-table'
 
 class Work extends React.Component {
   constructor(props) {
@@ -28,11 +29,6 @@ class Work extends React.Component {
       this.setState({ workData: res.data })
     })
   }
-/*A adapter*/
-  handleClick(rang){
-    const composedWorkId = (rang).slice(-36)
-    this.props.history.push('/work/'+composedWorkId)
-  }
 
   render() {
     if (!this.state.workData) {
@@ -41,63 +37,35 @@ class Work extends React.Component {
       return (
         <Container>
           <Grid>
-            <Typography variant='h5' component='h2'>
-              Affichage de l'oeuvre d'id <code>{this.props.match.params.id}</code>
-            </Typography>
-
-            {/*<Box mx='auto'>
+            <Box mx='auto'>
               <Typography variant='h6' component='h6'>
                 <Grid container direction='row' justify='space-evenly' alignItems='center'>
-                  <Box p={2}>{this.state.musicianData.surname}</Box>
-                  <Box p={2}>{this.state.musicianData.givenName}</Box>
-                  <Box p={2}>({this.state.musicianData.birthDate} - {this.state.musicianData.death_date})</Box>
+                  <Box p={2}>{this.state.workData[0].work_name}</Box>
+                  <Box p={2}>{this.state.workData[0].composer_given_name + " " + this.state.workData[0].composer_surname}</Box>
                 </Grid>
               </Typography>
-      </Box>*/}
+            </Box>
           </Grid>
-{/*
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              <Box mx='auto' direction='column'>
-                <Box>
-                  <Typography variant='button' component='h3' display="inline">
-                    statut :{" "}
-                  </Typography>
-                  <Typography variant='subtitle1' component='h3' display="inline">
-                    {this.state.musicianData.status_label}
-                  </Typography>
 
-                </Box>
-                <Box>
-                  <Typography gutterBottom variant='button' component='h3' display="inline">
-                    nationalite :{" "}
-                  </Typography>
-                  <Typography variant='subtitle1' component='h3' display="inline">
-                    {this.state.musicianData.nationality_label}
-                  </Typography>
+          <MaterialTable
+          title="Diffusions de l'oeuvre"
+          columns={[
+            { title : "Radio", field : "super_event_station_label"},
+            { title : "Jour", field : "super_event_jour_debut_diffusion"},
+            { title : "Horaire de début", field : "super_event_start_date", type : 'datetime'},
+            { title : "Horaire de fin", field : "super_event_end_date", type : 'datetime'},
+            { title : "Titre du programme", field : "super_event_title_label"},
+            { title : "Type du programme", field : "super_event_type_label"},
+            { title : "Format de diffusion", field : "super_event_format_label"},
+          ]}
+          data={this.state.workData}
+          onRowClick={((evt, selectedRow) => {
+            const progId = selectedRow.super_event.slice(-36)
+            this.props.history.push('/super_event/'+progId)
+          })}
+          >
 
-                </Box>
-                <Box>
-                  <Typography variant='button' component='h3' display="inline">
-                    style :{" "}
-                  </Typography>
-                  <Typography variant='subtitle1' component='h3' display="inline">
-                    {this.state.musicianData.style_label}
-                  </Typography>
-                </Box>
-              </Box>
-            </Grid>
-            <Grid item xs={6}>
-              <Box>
-                <Typography gutterBottom variant='button' component='h3'>
-                  Infos :
-                </Typography>
-                <Typography variant='subtitle1' component='h3'>
-                  {this.state.musicianData.description}
-                </Typography>
-              </Box>
-            </Grid>
-</Grid>*/}
+          </MaterialTable>
         </Container>
       )
     }

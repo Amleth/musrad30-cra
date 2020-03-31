841d73e3ca73fdd566017d7f091a7cc5c2cde821
plarocca <pierre.la-rocca@etu.utc.fr>
2020-03-31 12:20:52 +0200

    affichage clean des interpretes dans un programme

--- src/components/program/index.jsx
+++ src/components/program/index.jsx
@@ -12,6 +12,8 @@ import {
   TableCell,
   Typography
 } from '@material-ui/core'
+import MaterialTable from 'material-table'
+import lodash from 'lodash'
 
 class Program extends React.Component {
   constructor(props) {
@@ -21,8 +23,18 @@ class Program extends React.Component {
 
   componentDidMount() {
     const id = this.props.match.params.id
-    axios.get('http://data-iremus.huma-num.fr/musrad30/super_event/'+id).then(res => {
-      this.setState({ programData: res.data })
+    axios.get('http://data-iremus.huma-num.fr/musrad30/super_event/' + id).then(res => {
+      let newData = []
+      const data = lodash.groupBy(res.data, 'music_event')
+      console.log(data)
+      for (const work in data) {
+        const performers = data[work].map(item => 
+          (item.performer_surname ? (item.performer_given_name ? item.performer_given_name + " " : "") + item.performer_surname : null)
+          )
+          data[work][0].performer = performers
+          newData.push(data[work][0])
+        }
+        this.setState({ programData: newData })
     })
   }
 
@@ -38,7 +50,7 @@ class Program extends React.Component {
               <Typography variant='h6' component='h6'>
                 <Grid container direction='row' justify='space-evenly' alignItems='center'>
                   <Box p={2}>{pData.title_label}</Box>
-                  <Box p={2}>{pData.jour_debut_diffusion + " " + pData.start_date.slice(8,10)+"-"+pData.start_date.slice(5,7)+"-"+pData.start_date.slice(0,4)}</Box>
+                  <Box p={2}>{pData.jour_debut_diffusion + " " + pData.start_date.slice(8, 10) + "-" + pData.start_date.slice(5, 7) + "-" + pData.start_date.slice(0, 4)}</Box>
                   <Box p={2}>{pData.station_label}</Box>
                 </Grid>
               </Typography>
@@ -46,21 +58,53 @@ class Program extends React.Component {
           </Grid>
 
           <Grid item xs={6}>
-              <Box mx='auto' direction='column'>
-                <Box>
-                  <Typography variant='button' component='h3' display="inline">
-                    Détails :{" "}
-                  </Typography>
-                  <Typography variant='subtitle1' component='h3' display="inline">
-                    A Implémenter
-                  </Typography>
-                </Box>
+            <Box mx='auto' direction='column'>
+              <Box>
+                <Typography variant='button' component='h3' display="inline">
+                  Description :{" "}
+                </Typography>
               </Box>
-            </Grid>
+              <Box>
+                <Typography variant='subtitle1' component='h3' display="inline">
+                  A Implémenter
+                  </Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <MaterialTable
+          title="Plages diffusées"
+          columns={[
+            {title : "Titre de l'oeuvre", render : rowdata => {
+              if (rowdata.work_name) {
+                return rowdata.work_name
+              } else return ('Oeuvre anonyme')
+            }},
+            {title : "Compositeurs", render : r => {
+              if (r.composer) {
+                let name = (r.composer_given_name ? r.composer_given_name + " " : "" ) + r.composer_surname
+                return(name)
+              } else return ('Compositeur anonyme')
+            }},
+            {title : "Interprètes", render : r => {
+              if (r.performer[0]) {
+                console.log(r.performer.length)
+                let chaine = ""
+                for (let i = 0 ; i < (r.performer.length) - 1 ; i ++){
+                  chaine = chaine + r.performer[i] + ", "
+                }
+                chaine = chaine + r.performer[r.performer.length - 1]
+                return chaine
+              } else return ('Interprète anonyme')
+            }},
+          ]}
+          data={this.state.programData}
+          onRowClick={((evt, selectedRow) => {
+            const workId = selectedRow.work.slice(-36)
+            this.props.history.push('/work/'+workId)
+          })}
+          >
 
-            <Grid>
-              
-            </Grid>
+          </MaterialTable>
 
         </Container>
       )
